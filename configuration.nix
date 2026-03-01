ddd6920bacfe456eccca870b654026c25d124e22
Bitwise-Bandit <181691737+Bitwise-Bandit@users.noreply.github.com>
2026-03-01 07:57:39 +0530

    Nixos system files and home manager

---
+++ configuration.nix
@@ -0,0 +1,234 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      
+      # Include the home manager.
+      <home-manager/nixos>
+    ];
+
+  # Enabling experimental features(flakes)
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+   networking.hostName = "powerful-pc"; # Define your hostname.
+
+  # Configure network connections interactively with nmcli or nmtui.
+  networking.networkmanager.enable = true;
+
+  nixpkgs.config.allowUnfree = true;
+
+  # Set your time zone.
+   time.timeZone = "Asia/Kolkata";
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+   i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkb.options in tty.
+  # };
+
+  # Enable the X11 windowing system.
+  # services.xserver.enable = true;
+  services.xserver = {
+    enable = true;
+  };
+
+  # optimization of nix store
+  nix.optimise.automatic = true;
+  nix.optimise.dates = [ "daily" ];
+  
+  # automatic garbage collection with the nh helper
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = " --keep 10";
+  };
+
+
+  # Configure keymap in X11
+  # services.xserver.xkb.layout = "us";
+  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # services.pulseaudio.enable = true;
+  # OR
+   services.pipewire = {
+     enable = true;
+     pulse.enable = true;
+   };
+
+  # Enable sway
+   programs.sway = {
+     enable = true;
+     wrapperFeatures.gtk = true;
+     };
+  
+  # Enable gvfs and udevdisks2
+   services.udisks2.enable = true;
+   services.gvfs.enable = true;
+
+  # Enable gnome keyring and polkit
+   services.gnome.gnome-keyring.enable = true;
+   security.polkit.enable = true;
+
+  # Enable tlp
+   services.tlp.enable = true;
+  
+  # Enable libvirtd service
+   virtualisation.libvirtd.enable = true;
+  # Enable zram
+   zramSwap.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+   services.libinput.enable = true;
+
+  # Enable zsh
+   programs.zsh.enable = true;
+ 
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+   users.users.vis = {
+     isNormalUser = true;
+     shell = pkgs.zsh;
+     extraGroups = [ "wheel" "networkmanager" "qemu" "kvm" "libvirtd" "disk" "input"]; # Enable ‘sudo’ for the user.
+     packages = with pkgs; [
+       tree
+     ];
+   };
+ 
+  home-manager.useGlobalPkgs = true;
+ 
+home-manager.users.vis = ./home.nix;
+
+ # Set the environment variables
+  environment.variables = {
+    EDITOR = "nvim";
+  };
+
+   # programs.firefox.enable = true;
+
+
+  # List packages installed in system profile.
+  # You can use https://search.nixos.org/ to find more packages (and options).
+   environment.systemPackages = with pkgs; [
+     btop
+     binutils
+     fastfetch
+     fzf
+     git
+     gnupg
+     grim
+     kitty
+     kanata
+     libvirt
+     gcc
+     gnumake
+     mako
+     neovim
+     obsidian
+     qemu
+     ripgrep
+     slurp
+     swaybg
+     swaylock
+     swayidle
+     unzip
+     virt-manager
+     waybar
+     wl-clipboard
+     wofi
+     wget
+     zathura
+     zathuraPkgs.zathura_pdf_mupdf
+     ];
+
+  # Fonts
+  fonts.packages = with pkgs; [ 
+    font-awesome
+    nerd-fonts.jetbrains-mono 
+  ];
+
+  # remap capslock to escape
+
+   services.kanata = {
+    enable = true;
+    keyboards = {
+      "logi".config = ''
+(defsrc
+  caps
+)
+
+(deflayer colemak
+   (tap-hold 200 200 esc lctl)
+)
+  '';
+    };
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+   system.copySystemConfiguration = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "25.11"; # Did you read the comment?
+
+}
+
---
+++ home.nix
@@ -0,0 +1,119 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "vis";
+  home.homeDirectory = "/home/vis";
+
+  # Packages that should be installed to the user profile.
+ 
+  home.packages = with pkgs; [
+    qimgv
+    gammastep
+    librewolf
+    yazi
+  ];
+
+
+   #Enabling zsh and ohmyzsh
+   programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+      oh-my-zsh = {
+      enable = true;
+      plugins = [ "git" "fzf" ];
+      theme = "simple";
+  };
+
+};
+
+   #Enabling bash
+     programs.bash = {
+       enable = true;
+       enableCompletion = true;
+     };
+    
+    programs.starship = {
+      enable = true;
+      enableZshIntegration = true;
+      enableBashIntegration = true;
+
+      settings = {
+        add_newline = false;
+
+        # TWO LINES
+        format = ''
+          $directory$git_branch$git_status$nix_shell$cmd_duration
+          $character
+        '';
+
+        # DIRECTORY
+        directory = {
+          style = "purple";
+          truncation_length = 3;
+          truncate_to_repo = true;
+          truncation_symbol = "  ";
+        };
+
+        # Time
+        cmd_duration = { 
+          min_time = 2000;
+          };
+
+        # GIT STATUS 
+        git_status = {
+          style = "bold red";
+          format = "\\[$all_status$ahead_behind\\]($style)  ";   
+
+          staged = "S:$count ";
+          modified = "M:$count ";
+          untracked = "U:$count ";
+          deleted = "D:$count ";
+          renamed = "R:$count ";
+          conflicted = "C:$count ";
+          stashed = "T:$count ";
+        };
+
+       # PROMPT CHARACTER 
+        character = {
+          success_symbol = "[❯](bold green)";
+          error_symbol   = "[❯](bold red)";
+        };
+
+        # Additional symbols
+        git_branch.symbol = "";
+        # nix_shell.symbol  = " ";
+        nix_shell = {
+          format = "[$symbol]($style) ";
+        };
+
+        cmd_duration.disabled = false;
+      };
+    };
+
+
+  services.gpg-agent = {
+    enable = true;
+    defaultCacheTtl = 1800;
+    enableSshSupport = false;
+  };
+
+  services.ssh-agent.enable = true;
+    
+   # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "25.11";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  }
