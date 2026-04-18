a31205a2bca25094b7bf6f654c8ed542548443cd
Bitwise-Bandit <181691737+Bitwise-Bandit@users.noreply.github.com>
2026-04-18 14:12:53 +0530

    modular structure and flakes

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "dotfiles";
+
+  inputs = {
+    unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    home-manager = {
+      # url-unstable = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+ 
+ outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      ...
+    }@inputs:
+    {
+      nixosConfigurations = {
+        vis = nixpkgs.lib.nixosSystem {
+          specialArgs = {
+            inherit inputs;
+          };
+          modules = [
+            ./hosts/vis/configuration.nix
+          ];
+        };
+      };
+    };
+}
